32d4176fd5ed453c44a6fdb2b9a0671781b971a5
JoGom <jogoram428@gmail.com>
2019-04-22 00:01:55 -0700

    first commit

---
+++ models/student.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const studentSchema = new Schema({
+    name: { type: String, required: true },
+    age: { type: Number, required: true },
+    image: { type: String, default: "/assets/images/no-image.png"},
+    behaviors: [{
+        behavior: {type: String},
+        
+    }],
+    commentArr: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Comment'
+    }] 
+
+}, {strict: false});
+
+const Student = mongoose.model("Student", studentSchema);
+
+module.exports = Student;
+
+// parents: [{
+//     parent1: {
+//         type: String
+//     },
+//     parent2: {
+//         type: String
+//     }
+// }]
